1f8a3da0eadd28c9cd7cfe6e527c3cb7b255b826
Guled Abdalla <55400003+github8585@users.noreply.github.com>
2023-03-23 22:48:47 +0000

    Update threat.py

--- threat.py
+++ threat.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-url = 'https://www.virustotal.com/api/v3/files/c386396ec70db3608075b5fbfaac4ab1ccaa86ba05a68ab393ec551eb66c3e00'
-headers = {'x-apikey': '6a9b731b043700d70c9fa646d490e7dcd8132107d76bc8e46bd84b2b438308c7'}
+url = 'https://www.virustotal.com/api/v3/files/file_id'
+headers = {'x-apikey': 'api-key'}
 
 response = requests.get(url, headers=headers)
 
